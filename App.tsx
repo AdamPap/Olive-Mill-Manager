d604e01391f173d81a76f87845875226903e303a
Adam <adampapamast@hotmail.com>
2023-01-10 20:41:53 +0200

    add: realm provider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {extendTheme, NativeBaseProvider, useColorMode} from 'native-base';
+import {extendTheme, NativeBaseProvider} from 'native-base';
 import React from 'react';
+
 import CreateRoute from './src/screens/CreateRoute';
 import Home from './src/screens/Home';
 import ProfileScreen from './src/screens/ProfileScreen';
+
+import {RealmContext} from './src/models';
+const {RealmProvider} = RealmContext;
 
 export type RootStackParamList = {
   Home: undefined;
@@ -22,28 +26,30 @@ const customTheme = extendTheme(config);
 
 const App = () => {
   return (
-    <NativeBaseProvider theme={customTheme}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerTitleAlign: 'center',
-          }}>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{title: 'Olive Mill Manager'}}
-          />
-          <Stack.Screen
-            name="CreateRoute"
-            component={CreateRoute}
-            // initialParams={{userId: 2}}
-            options={{title: 'Create Route'}}
-          />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <RealmProvider>
+      <NativeBaseProvider theme={customTheme}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              headerTitleAlign: 'center',
+            }}>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{title: 'Olive Mill Manager'}}
+            />
+            <Stack.Screen
+              name="CreateRoute"
+              component={CreateRoute}
+              // initialParams={{userId: 2}}
+              options={{title: 'Create Route'}}
+            />
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </RealmProvider>
   );
 };
 
