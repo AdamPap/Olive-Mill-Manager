3f20bacc26d5b01beadaa38717fcf9a2cd51c751
Adam <adampapamast@hotmail.com>
2023-02-27 20:19:51 +0200

    add: switch default to custom AppBar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {extendTheme, NativeBaseProvider} from 'native-base';
-import React, {UIEventHandler} from 'react';
+import React from 'react';
 
 import CreateRoute from './src/screens/CreateRoute';
 import Home from './src/screens/Home';
 import ProfileScreen from './src/screens/ProfileScreen';
 
+import AppBar from './src/components/AppBar';
 import {RealmContext} from './src/models';
 import AllRoutes from './src/screens/AllRoutes';
 const {RealmProvider} = RealmContext;
@@ -34,24 +35,11 @@ const App = () => {
           <Stack.Navigator
             initialRouteName="Home"
             screenOptions={{
-              headerTitleAlign: 'center',
+              header: props => <AppBar title={props.route.name} {...props} />,
             }}>
-            <Stack.Screen
-              name="Home"
-              component={Home}
-              options={{title: 'Olive Mill Manager'}}
-            />
-            <Stack.Screen
-              name="CreateRoute"
-              component={CreateRoute}
-              // initialParams={{userId: 2}}
-              options={{title: 'Προσθήκη Νέου Χωραφιού'}}
-            />
-            <Stack.Screen
-              name="AllRoutes"
-              component={AllRoutes}
-              options={{title: 'All Routes'}}
-            />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="CreateRoute" component={CreateRoute} />
+            <Stack.Screen name="AllRoutes" component={AllRoutes} />
             <Stack.Screen name="Profile" component={ProfileScreen} />
           </Stack.Navigator>
         </NavigationContainer>
